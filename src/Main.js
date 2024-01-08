8428b5adbeb58dcc8a5ddfbf51d6075b66bdcbe5
tgrall <tugdual@gmail.com>
2024-01-08 13:44:37 +0100

    solutions

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import User from './User';
+
+function Main() {
+  return (
+    <div>
+      <Header />
+      <User />
+    </div>
+  );
+}
+
+export default Main;
